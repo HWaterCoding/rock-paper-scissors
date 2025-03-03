41cba6a47b5056217f0891bcd8628d098c3dc108
HWaterCoding <183124787+HWaterCoding@users.noreply.github.com>
2025-03-03 17:51:31 -0500

    Create html and script files Create rough layout of functions Write pseudocode to plan out process of functions

---
+++ script.js
@@ -0,0 +1,27 @@
+function getComputerChoice(){
+    // create a variable for the computers choice
+    // assign a math.random method to the variable
+    // return string of rock paper or scissors randomly
+}
+// console log the return value
+
+function getHumanChoice(){
+    // create a variable for the humans choice
+    // assign a prompt to the variable to ask for input
+    // based on the users input, return the corresponding string
+}
+//console log the returned value
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice){
+    // make humanChoice parameter case-insensitive
+    // create conditional that evalutes if human won or lost
+    // console log a winning/losing message based on result
+    // if human wins, increment humanScore by 1.
+    // if computer wins, increment computerScore by 1.
+}
+
+let humanChoice = 0
+let computerChoice = 0
