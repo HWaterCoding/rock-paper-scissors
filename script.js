bddf07014ffbb97c4cceeb52bbcbc575f3d84fb2
HWaterCoding <183124787+HWaterCoding@users.noreply.github.com>
2025-03-05 21:17:58 -0500

    Create first function for random computer choice Begin working out second function for human choice

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 function getComputerChoice(){
-    // create a variable for the computers choice
-    // assign a math.random method to the variable
-    // return string of rock paper or scissors randomly
+    let computerChoice = Math.floor(Math.random() * 3);
+    switch (computerChoice){
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        case 2:
+            return "scissors";
+    }
 }
+console.log(getComputerChoice());
 // console log the return value
 
 function getHumanChoice(){
+    let userChoice = prompt("Rock, Paper, or Scissors?");
+
+    if (userChoice = "Rock"){
+        console.log("Rock");
+    } else if (userChoice = "Paper"){
+        console.log("Paper");
+    } else if (userChoice = "Scissors"){
+        return "Scissors";
+    } else {
+        return "Invalid Option";
+    }
     // create a variable for the humans choice
     // assign a prompt to the variable to ask for input
     // based on the users input, return the corresponding string
